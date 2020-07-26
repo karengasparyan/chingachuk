b91993de14235ba160a49e530eb552991229a10f
Karen <karengasparyan.a@gmail.com>
2020-07-27 00:13:35 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ching from "./pages/Ching";
+
+function App() {
+  return (
+    <>
+     <Ching />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Ching.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Ching extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return(
+            <h1>hello</h1>
+        )
+    }
+}
+
+export default Ching;
