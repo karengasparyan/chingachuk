0bab48938da98ece769d27fb5c6883d6964a204e
Karen <karengasparyan.a@gmail.com>
2020-07-28 13:43:09 +0400

    jul/28/13:44

--- src/pages/Prs.js
+++ src/pages/Prs.js
@@ -13,8 +13,8 @@ class Prs extends Component {
             ],
             playerPrs: '',
             botPrs: '',
-            playerCount: JSON.parse(localStorage.getItem('playerCount')) || [],
-            botCount: JSON.parse(localStorage.getItem('botCount')) || [],
+            playerCount: JSON.parse(localStorage.getItem('playerCount')) || 0,
+            botCount: JSON.parse(localStorage.getItem('botCount')) || 0,
             winDesc: '',
             prsWin: '',
         }
@@ -35,22 +35,27 @@ class Prs extends Component {
         return prsRandArray[randPrsNum]
     }
 
-    resetScore = () =>{
+    resetScore = () => {
         localStorage.removeItem('playerCount')
         localStorage.removeItem('botCount')
         this.setState({
-            playerCount: JSON.parse(localStorage.getItem('playerCount')) || [],
-            botCount: JSON.parse(localStorage.getItem('botCount')) || [],
+            playerPrs: '',
+            botPrs: '',
+            playerCount: JSON.parse(localStorage.getItem('playerCount')) || 0,
+            botCount: JSON.parse(localStorage.getItem('botCount')) || 0,
+            winDesc: '',
+            prsWin: '',
         })
     }
 
     calculate = (playerPrs, botPrs) => {
         const {playerCount, botCount} = this.state
         if (playerPrs.name === botPrs.name) {
-            this.setState({
-                winDesc: 'NO BODY WON',
-                prsWin: {name: 'NOBODY WON'},
+            return this.setState({
+                prsWin: {name: 'NO BODY WON'},
+                winDesc: ''
             })
+
         }
         if (playerPrs.name === "rock") {
             if (botPrs.name === "scissors") {
@@ -105,41 +110,49 @@ class Prs extends Component {
         }
     }
 
-
     render() {
         const {prs, playerCount, winDesc, botCount, prsWin, playerPrs, botPrs} = this.state
         return (
             <div>
                 <div className="prsContainer">
-                    {prs.map((prs, i) => (
-                        <div onClick={() => this.changePlayerPrs(prs)} key={i} className="prs">
-                            <img src={prs.img} alt={prs.name}/>
-                        </div>))}
-                </div>
-                <div>
-                    <span><h1>PLAYER </h1></span>
-                    <img
-                        className={winDesc === 'YOU WIN' ? 'prsImgWin' : 'prsImgLose'}
-                        src={playerPrs.img}
-                        alt={playerPrs.name}
-                    />
-                    {prsWin.name === 'NOBODY WON' ? <h1>NOBODY WON</h1> : <fragment>
-                        <span><h1>WIN </h1></span>
-                        <img className="prsImgWinDesc"
-                             src={prsWin.img}
-                             alt={prsWin.name}/>
-                    </fragment>}
-                    <span><h1>BOT </h1></span>
-                    <img className={winDesc === 'YOU LOSE' ? 'prsImgWin' : 'prsImgLose'}
-                         src={botPrs.img}
-                         alt={botPrs.name}/>
-                    <h1>Player: {playerCount}</h1>
-                    <h1>Bot: {botCount}</h1>
-                    <h1>{winDesc} {prsWin && `Win ${prsWin.name}`}</h1>
+                    <div className="prsWinArea">
+                        <div className="prsArea">
+                            {playerPrs === '' ? null : <span>PLAYER</span>}
+                            <img
+                                className={winDesc === 'YOU WIN' ? 'prsImgWin' : 'prsImgLose'}
+                                src={playerPrs.img}
+                                alt={playerPrs.name}
+                            />
+                        </div>
+                        <div className="prsArea">
+                            {prsWin.name === 'NO BODY WON' ? <span style={{marginLeft: 30}}>{prsWin.name}</span> : <>
+                                {playerPrs === '' ? null : <span>WIN</span>}
+                                <img className="prsImgWinDesc"
+                                     src={prsWin.img}
+                                     alt={prsWin.name}/>
+                            </>}
+                        </div>
+                        <div className="prsArea">
+                            {playerPrs === '' ? null : <span>BOT</span>}
+                            <img className={winDesc === 'YOU LOSE' ? 'prsImgWin' : 'prsImgLose'}
+                                 src={botPrs.img}
+                                 alt={botPrs.name}/>
+                        </div>
+                    </div>
+                    <div className="PrsWinCounts">
+                        <span>Player: {playerCount}</span>
+                        <span>Bot: {botCount}</span>
+                        <span>{winDesc} {winDesc && ` ${prsWin.name} win`}</span>
+                    </div>
+                    <div className="prsPlayer">
+                        {prs.map((prs, i) => (
+                            <div onClick={() => this.changePlayerPrs(prs)} key={i}>
+                                <img src={prs.img} alt={prs.name}/>
+                            </div>))}
+                    </div>
                     <button onClick={this.resetScore}>reset score</button>
                 </div>
             </div>
-
         )
     }
 }
